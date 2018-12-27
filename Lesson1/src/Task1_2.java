02ec2f6923ad30437594a6bbee01131faa644009
NikW-v <Nikolai1193@gmail.com>
2018-12-27 10:34:31 +0300

    Задание 1-2

---
+++ Lesson1/src/Task1_2.java
@@ -0,0 +1,16 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+
+        byte b = 127;
+        short s = 10;
+        int i = 500;
+        long l = 75452345L;
+
+        float f = 235.4f;
+        double d = 43.543245;
+
+        boolean a = true;
+
+        char c = 'A';
+    }
+}
