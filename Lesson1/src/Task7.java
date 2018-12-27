8d4357789473d2a0848a58d2c96171102da974fb
NikW-v <Nikolai1193@gmail.com>
2018-12-27 10:46:58 +0300

    Задаине 7

---
+++ Lesson1/src/Task7.java
@@ -0,0 +1,12 @@
+public class Task7 {
+    public static void main(String[] args) {
+
+        String name = "Name";
+
+        printName(name);
+    }
+
+    public static void printName(String name){
+        System.out.println("Hello " + name);
+    }
+}
