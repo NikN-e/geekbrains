b117d5ca1c3983a6f545bfcc296bd3f9a579415f
NikW-v <Nikolai1193@gmail.com>
2018-12-27 10:44:56 +0300

    Задание 5

---
+++ Lesson1/src/Task5.java
@@ -0,0 +1,16 @@
+public class Task5 {
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        comparison(a);
+    }
+
+    public static void comparison(int a){
+        if (a >= 0){
+            System.out.println("Число положительное");
+        }else{
+            System.out.println("Число отрицательное");
+        }
+    }
+}
