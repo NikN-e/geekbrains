2e0c3ab06211e24c0198d09a31624b03a52544f6
NikW-v <Nikolai1193@gmail.com>
2018-12-27 10:45:54 +0300

    Задание 6

---
+++ Lesson1/src/Task6.java
@@ -0,0 +1,13 @@
+public class Task6 {
+    public static void main(String[] args) {
+
+        int a = 1;
+
+
+        System.out.println(check(a));
+    }
+
+    public static boolean check(int a){
+        return a < 0;
+    }
+}
