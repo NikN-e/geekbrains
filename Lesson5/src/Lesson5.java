df495db5e23734a26d1897d6073f59d057aae96b
NikW <Nikolai1193@gmail.com>
2019-01-23 20:02:01 +0300

    Lesson5

--- Lesson5/src/Lesson5.java
+++ Lesson5/src/Lesson5.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Lesson5 {
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        worker.Worker();
+        System.out.println();
+
+        Worker[] workArr = new Worker[5];
+        workArr[0] = new Worker("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        workArr[1] = new Worker("Petrov Petr", "Engineer", "pertov@mai.box.com", "894563247", 35000, 32);
+        workArr[2] = new Worker("Sidorov Vasilyi", "Master", "sidorov@mailbox.com", "894163846", 25000, 41);
+        workArr[3] = new Worker("Pupkin Vasyli", "Master", "pupkin@mailbox.com", "891547538", 20000, 45);
+        workArr[4] = new Worker("Petrov Ivan", "Master", "petrovivan@mailbox.com", "897635327", 23000, 35);
+
+        for (int i = 0; i < workArr.length; i++) {
+            if(workArr[i].age > 40){
+               System.out.println(workArr[i].fio + " " + workArr[i].post + " " + workArr[i].e_mail + " " + workArr[i].phone + " " + workArr[i].salary + " "+ workArr[i].age);
+
+            }
+
+        }
+    }
 }
--- Lesson5/src/Worker.java
+++ Lesson5/src/Worker.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Worker {
+    String fio;
+    String post;
+    String e_mail;
+    String phone;
+    double salary;
+    int age;
+
+    public Worker(){
+        fio = "Petrov Petr";
+        post = "Engineer";
+        e_mail = "Petrov@mail";
+        phone = "63-42-57";
+        salary = 20000;
+        age = 20;
+    }
+
+    public Worker(String fio, String post, String e_mail, String phone, double salary, int age){
+        this.fio = fio;
+        this.post = post;
+        this.e_mail = e_mail;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void Worker(){
+        System.out.println(fio + " " + post + " " + e_mail + " " + phone + " " + salary + " " + age);
+    }
 }
